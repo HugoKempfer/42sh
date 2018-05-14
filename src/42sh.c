17c6667e60be715e00f5e30371abbdbda56268bc
Hugo Kempfer <hugo.kempfer@epitech.eu>
2018-05-14 22:45:38 +0200

    Adding base files for env management

--- src/42sh.c
+++
@@ -1,8 +0,0 @@
-/*
-** EPITECH PROJECT, 2018
-** 42sh
-** File description:
-** Main 42sh loop
-*/
-
-
---
+++ src/init/env.c
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2018
+** 42sh
+** File description:
+** Initialization of the env list
+*/
+
+#include "42sh"
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Example of the tcsh vars
+ 	HOSTTYPE=x86_64-linux
+	VENDOR=unknownOSTYPE=linuxMACHTYPE=x86_64
+	SHLVL=1
+	PWD=/home/hugo
+	LOGNAME=hugo
+	USER=hugo
+	GROUP=users
+	HOST=DarkCake
+*/
+
+static int copy_base_env(char **base_env, llist_t *env)
+{
+	lnode_t *var = env->head;
+
+	while (base_env) {
+		if (!list_push_tail(*base_env, env)) {
+			return (false);
+		}
+		++base_env;
+	}
+	return (true);
+}
+
+int init_list(char **base_env, llist_t *env)
+{
+	if (!copy_base_env(base_env, env)) {
+		return (false);
+	}
+	//TODO add base TCSH vars if not existing
+	return (true);
+}
