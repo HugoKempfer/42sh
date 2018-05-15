85aca192b6c2a6ee08482671891c1f3464cb183e
Lidin <nicolas.lidin@epitech.eu>
2018-05-15 18:43:18 +0200

    add init env -> to init

--- src/init/env.c
+++ src/init/env.c
@@ -12,15 +12,10 @@
 #include <stdlib.h>
 
 /* Example of the tcsh vars
- 	HOSTTYPE=x86_64-linux
-	VENDOR=unknownOSTYPE=linuxMACHTYPE=x86_64
-	SHLVL=1
 	PWD=/home/hugo
-	LOGNAME=hugo
+	OLDPWD=/
 	USER=hugo
 	GROUP=users
-	HOST=DarkCake
-	OLDPWD=/
 	HOME=/home/
 */
 
@@ -28,7 +23,7 @@ static int copy_base_env(char **base_env, llist_t *env)
 {
 	lnode_t *var = env->head;
 
-	while (base_env) {
+	while (*base_env) {
 		if (!list_push_tail(*base_env, env)) {
 			return (false);
 		}
@@ -37,11 +32,56 @@ static int copy_base_env(char **base_env, llist_t *env)
 	return (true);
 }
 
+static int exist_var(llist_t *env, char *var_name)
+{
+	node_t *var = env->head;
+
+	while (var) {
+		if (!strncmp(var->data, var_name, my_strlen(var_name))) {
+			return (true);
+		}
+		var = var->next;
+	}
+	return (false);
+}
+
+int push_variable_env(llist_t *env, char *name, char *var_value)
+{
+	char *variable = NULL;
+
+	if (!exist_var(env, name)) {
+		variable = str_concat((char *[]){name, "=", var_value});
+		if (variable && !list_push_tail(variable, env)) {
+			return (false);
+		}
+	}
+	return (true);
+}
+
+int add_basic_variables_env(llist *env)
+{
+	struct passwd *pw = getpwuid(getuid());
+
+	if (!push_variable_env(env, "USER", pw->pw_uid)) {
+		return (false);
+	}
+	if (!push_variable_env(env, "HOME", pw->pw_dir)) {
+		return (false);
+	}
+	if (!push_variable_env(env, "PATH", "/bin:/usr/bin:/usr/local/bin")) {
+		return (false);
+	}
+	/*getcwd(NULL, un chiffre)*/
+	return (true);
+}
+
 int init_list(char **base_env, llist_t *env)
 {
 	if (!copy_base_env(base_env, env)) {
 		return (false);
 	}
-	//TODO add base TCSH vars if not existing
+	if (!add_basic_variables_env(env)) {
+		return (false);
+	}
 	return (true);
 }
