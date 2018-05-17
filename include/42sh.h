68f0a694396100d40e85c0d0864f0f0b1adffcac
Lidin <nicolas.lidin@epitech.eu>
2018-05-17 16:18:47 +0200

    add struct shell_path

--- include/42sh.h
+++ include/42sh.h
@@ -11,12 +11,20 @@
 #include "list.h"
 
 typedef struct shell_info_s shell_info_t;
+typedef struct shell_path_s shell_path_t;
+
+struct shell_path_s {
+	char *pwd;
+	char *old_pwd;
+	char *home;
+};
 
 struct shell_info_s {
 	llist_t *processes;
 	llist_t *global_vars;
 	llist_t *local_vars;
 	llist_t *env;
+	shell_path_t path;
 };
 
 shell_info_t *init_shell_info(void);
