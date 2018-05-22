88fe8d1d2c15163bda1821eb6daba86b4cdd7636
Hugo Kempfer <hugo.kempfer@epitech.eu>
2018-05-22 11:41:58 +0200

    Commiting before branching redirections functions

---
+++ include/redirections.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** 42sh
+** File description:
+** Manage binary fd redirections
+*/
+
+#ifndef REDIRECTIONS_H
+#define REDIRECTIONS_H
+
+#endif /* ifndef REDIRECTIONS_H */
--- src/command_interpreter/pipe.c
+++ src/command_interpreter/pipe.c
@@ -5,6 +5,10 @@
 ** Handle pipe redirections
 */
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "redirections.h"
 #include "binary_tree.h"
 #include "42sh.h"
