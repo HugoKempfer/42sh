f57a940a2bed18f6b0d0736a93bddfafa42e102d
Stéphane Dubedat <Thewebgrenier@mailoo.org>
2018-05-30 17:39:18 +0200

    update the var builtin

--- src/var/var_helpers.c
+++ src/var/var_helpers.c
@@ -8,35 +8,62 @@
 #include "functions.h"
 #include "list.h"
 #include "var.h"
+#include "42sh.h"
 
-bool set_var(llist_t *var_list, const char *name, const char *value)
+size_t grid_len(char **grid)
 {
-	lnode_t *node = var_list->head;
+	size_t word_idx = 0;
+
+	if (!grid) {
+		return (0);
+	}
+	while (grid[word_idx++]);
+	return (--word_idx);
+}
+
+bool is_setting_the_var(char **cmd, char **name_or_value, shell_info_t *info)
+{
+	if (!strcmp(cmd[0], "set") && grid_len(cmd) >= 2) {
+		if (grid_len(cmd) == 4 && !strcmp(cmd[2], "=")) {
+			name_or_value[1] = cmd[3];
+			return (true);
+		}
+		else if (grid_len(cmd) == 2) {
+			name_or_value[1] = NULL;
+			return (true);
+		}
+	}
+	return (false);
+}
+
+bool set_var(shell_info_t *info, char **name_or_value)
+{
+	lnode_t *node = info->local_vars->head;
 	var_t *var = NULL;
 
 	while (node) {
-		if (!strcmp(((var_t *)node->data)->name, name)) {
+		if (!strcmp(((var_t *)node->data)->name, name_or_value[0])) {
 			free(((char *)((var_t *)node->data)->value));
-			((var_t *)node->data)->value = value;
+			((var_t *)node->data)->value = name_or_value[1];
 			return (true);
 		}
 		node = node->next;
 	}
-	var = init_struct_var(name, value);
-	if (!list_push_tail(var, var_list)) {
+	var = init_struct_var(name_or_value[0], name_or_value[1]);
+	if (!list_push_tail(var, info->local_vars)) {
 		return (false);
 	}
 	return (true);
 }
 
-bool remove_var(llist_t *var_list, const char *name)
+bool remove_var(shell_info_t *info, char **name_or_value)
 {
-	lnode_t *node = var_list->head;
+	lnode_t *node = info->local_vars->head;
 	var_t *var = NULL;
 
 	while (node) {
-		if (!strcmp(((var_t *)node->data)->name, name)) {
-			list_pop(node, var_list);
+		if (!strcmp(((var_t *)node->data)->name, name_or_value[0])) {
+			list_pop(node, info->local_vars);
 			return (true);
 		}
 		node = node->next;
